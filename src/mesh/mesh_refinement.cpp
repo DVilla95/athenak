4ec8ea37cbf4a50f924d36b3e19f3a51e6d38e75
Daniele Villa <daniele.villa@ipp.mpg.de>
2025-09-30 16:44:41 +0200

    Fix wrong type for input parameter.

--- src/mesh/mesh_refinement.cpp
+++ src/mesh/mesh_refinement.cpp
@@ -56,7 +56,7 @@ MeshRefinement::MeshRefinement(Mesh *pm, ParameterInput *pin) :
   if (pin->DoesBlockExist("mesh_refinement")) {
     // read interval (in cycles) between check of AMR and derefinement
     ncyc_check_amr = pin->GetOrAddReal("mesh_refinement", "ncycle_check", 1);
-    refinement_interval = pin->GetOrAddReal("mesh_refinement", "refinement_interval", 5);
+    refinement_interval = pin->GetOrAddInteger("mesh_refinement", "refinement_interval", 5);
     // read prolongate primitives flag
     if (pin->DoesParameterExist("mesh_refinement", "prolong_primitives")) {
       prolong_prims = pin->GetBoolean("mesh_refinement", "prolong_primitives");
