f4b5483194b4233f8438de83c4328e4d5792ebfa
Daniele Villa <daniele.villa@ipp.mpg.de>
2026-02-24 16:11:40 +0100

    Change cost estimate by normalizing for number of cells.

--- src/mesh/mesh_refinement.cpp
+++ src/mesh/mesh_refinement.cpp
@@ -566,7 +566,7 @@ void MeshRefinement::RedistAndRefineMeshBlocks(ParameterInput *pin, int nnew, in
     auto ms_idcs = pmy_mesh->mesh_indcs;
     float ncells_per_mb = ms_idcs.nx1*ms_idcs.nx2*ms_idcs.nx3;
     const Real prt_cost = ppart->prtcl_cost;
-    for (int i=0; i<new_nmb; i++) {new_cost_eachmb[i] += prt_cost*gather_ppmb[newtoold[i]];}
+    for (int i=0; i<new_nmb; i++) {new_cost_eachmb[i] += prt_cost*gather_ppmb[newtoold[i]]/ncells_per_mb;}
   }
   pm->LoadBalance(new_cost_eachmb, new_rank_eachmb, new_gids_eachrank, new_nmb_eachrank,
                   new_nmb_total);
